580ca79d7ac190b53d0520d9cef9b086fd87bfc5
Nirmal Anilkumar <83705666+N1RMAL@users.noreply.github.com>
2022-05-16 15:38:46 +0530

    Create string_functions.dart

---
+++ string_functions.dart
@@ -0,0 +1,8 @@
+void main()
+{
+  String a = '   hello from the other side';
+  print(a.contains('side'));
+  print(a.replaceAll('hello','goodbye'));
+  print(a.trim());
+  print(a.toUpperCase());
+}
